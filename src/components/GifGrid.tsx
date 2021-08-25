a5f2d2ed1c6543eda2f2ef29d75ceb7af374c823
Ángel Cruz <langelcruzlara@gmail.com>
2021-08-25 12:04:48 -0500

    feat: adding container to GridItems

---
+++ src/components/GifGrid.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { APIResponse, GifInterface } from '../interfaces/ApiResponse';
+import { CustomGif } from '../interfaces/CustomGif';
+import GridItem from './GridItem';
+
+function GifGrid({ category }: { category: string }) {
+  const [images, setImages] = useState<CustomGif[]>([]);
+
+  useEffect(() => {
+    getGifs();
+  }, []);
+
+  const getGifs = async () => {
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=bcK9mKzbWV28yu4f9MD8uTnkdK9eYTRo&q=rick+and+morty&limit=10`;
+    const resp = await fetch(url);
+    const { data }: APIResponse = await resp.json();
+
+    const gifs = data.map((gif: GifInterface): CustomGif => {
+      return {
+        id: gif.id,
+        title: gif.title,
+        url: gif.images?.downsized_medium.url,
+      };
+    });
+
+    setImages(gifs);
+  };
+
+  return (
+    <div>
+      <h3>{category}</h3>
+      {images.map(({ id, title, url }) => (
+        <GridItem key={id} title={title} url={url} id={id} />
+      ))}
+    </div>
+  );
+}
+
+export default GifGrid;
